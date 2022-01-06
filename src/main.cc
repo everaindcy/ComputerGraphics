9b8723f5190e76d6be729e655ad5e7f0d3d0e223
dcy@temporary <temporary@temporary>
2022-01-06 23:52:40 +0800

    finish 11

--- src/main.cc
+++ src/main.cc
@@ -1,10 +1,10 @@
 #include "utils/utils.h"
 #include "object/objects.h"
 #include "material/materials.h"
-
-#include "camera.h"
+#include "unit/units.h"
 
 #include <iostream>
+
 color ray_color(const ray& r, const hittable& world, int depth) {
     hit_record rec;
 
@@ -38,7 +38,7 @@ int main() {
     // World
 
     hittable_list world;
-    
+
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
     auto material_left   = make_shared<dielectric>(1.5);
@@ -47,11 +47,10 @@ int main() {
     world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
     world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
-    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),  -0.4, material_left));
+    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0), -0.45, material_left));
     world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
 
-    // Camera
-    camera cam;
+    camera cam(point3(-2,2,1), point3(0,0,-1), vec3(0,1,0), 20, aspect_ratio);
 
     // Render
 
---
+++ src/unit/units.h
@@ -0,0 +1,6 @@
+#ifndef UNITS_H
+#define UNITS_H
+
+#include "camera.h"
+
+#endif
